6597c0a685168887c7d89a7dac8e27a918d74c2d
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2023-03-01 15:28:30 +0100

    add missing exports

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ export { default as MemFilesApi } from "./MemFilesApi.js";
 export { default as NodeFilesApi } from "./NodeFilesApi.js";
 export { default as BrowserFilesApi } from "./BrowserFilesApi.js";
 export { default as normalizePath } from "./normalizePath.js";
+
+export { default as getMimeType } from "./getMimeType.js";
+export * from "./mimeTypes.js";
