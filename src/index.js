e4d7014b30e59fc6ce978b6631c66a64d0bc92c5
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2023-03-08 15:00:00 +0100

    add a log wrapper for FilesApi instances

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export { default as MemFilesApi } from "./MemFilesApi.js";
 export { default as NodeFilesApi } from "./NodeFilesApi.js";
 export { default as BrowserFilesApi } from "./BrowserFilesApi.js";
 export { default as normalizePath } from "./normalizePath.js";
+export { default as addFilesApiLogger } from "./addFilesApiLogger.js";
 
 export { default as getMimeType } from "./getMimeType.js";
 export * from "./mimeTypes.js";
