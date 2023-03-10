dd0be485288ec89d35f43d1adeffc9cb8a99e626
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2023-03-10 23:05:25 +0100

    refactor: move common tests in a separate package (@statewalker/webrun-files-test)

--- test/MemFilesApiTest.js
+++ test/MemFilesApiTest.js
@@ -1,11 +1,11 @@
 import { MemFilesApi } from "../src/index.js";
 import { webcrypto } from "node:crypto";
-import runFileSystemTests from "./runFileSystemTests.js";
+import { runFilesApiTests } from "@statewalker/webrun-files-tests"
 import expect from "expect.js";
 
-runFileSystemTests({
+runFilesApiTests({
   expect,
   crypto : webcrypto,
   name: "MemFilesApi",
-  newFileApi: () => new MemFilesApi({}),
+  newFilesApi: () => new MemFilesApi({}),
 });
--- test/NodeFilesApiTest.js
+++ test/NodeFilesApiTest.js
@@ -1,14 +1,14 @@
 import { NodeFilesApi } from "../src/index.js";
 import fs from "node:fs/promises";
 import { webcrypto } from "node:crypto";
-import runFileSystemTests from "./runFileSystemTests.js";
 import expect from "expect.js";
+import { runFilesApiTests } from "@statewalker/webrun-files-tests"
 
-runFileSystemTests({
+runFilesApiTests({
   expect,
   crypto: webcrypto,
   name: "NodeFilesApi",
-  newFileApi: () =>
+  newFilesApi: () =>
     new NodeFilesApi({
       fs,
       rootDir: new URL("./test-data", import.meta.url).pathname,
