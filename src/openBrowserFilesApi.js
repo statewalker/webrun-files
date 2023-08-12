81885b46d43588fd52c2930ad8918ed5386cadca
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2023-08-12 16:51:21 +0200

    refactor: export utility methods for browser-based file system

--- src/openBrowserFilesApi.js
+++ src/openBrowserFilesApi.js
@@ -1,6 +1,6 @@
 import BrowserFilesApi from "./BrowserFilesApi.js";
 
-export default async function openBrowserFilesApi({
+export async function openBrowserFilesApi({
   handlerKey = "root-dir",
   readwrite = true,
   // Methods below could be replaced by persistent implementation
@@ -24,44 +24,44 @@ export default async function openBrowserFilesApi({
     throw new Error("Can not access to the folder. Please try again.");
   }
   return new BrowserFilesApi({ rootHandler });
+}
 
-  async function isHandlerAccessible(fileHandle) {
-    // We need to perform a real read operation with the handler
-    // to check if the file/directory still exists.
-    let exists = false;
-    try {
-      if (fileHandle.kind === "file") {
-        await fileHandle.getFile();
-      } else {
-        for await (let item of fileHandle.values()) {
-          break;
-        }
+export async function isHandlerAccessible(fileHandle) {
+  // We need to perform a real read operation with the handler
+  // to check if the file/directory still exists.
+  let exists = false;
+  try {
+    if (fileHandle.kind === "file") {
+      await fileHandle.getFile();
+    } else {
+      for await (let item of fileHandle.values()) {
+        break;
       }
-      exists = true;
-    } catch (error) {
-      console.log(error, fileHandle);
-      exists = false;
     }
-    return exists;
+    exists = true;
+  } catch (error) {
+    console.log(error, fileHandle);
+    exists = false;
   }
+  return exists;
+}
 
-  async function verifyPermission(fileHandle, readWrite) {
-    const options = {};
-    if (readWrite) {
-      options.mode = "readwrite";
-    }
-    let granted = false;
-    // Check if permission was already granted. If so, return true.
-    if ((await fileHandle.queryPermission(options)) === "granted") {
-      granted = true;
-    }
-    // Request permission. If the user grants permission, return true.
-    if (
-      !granted && (await fileHandle.requestPermission(options)) === "granted"
-    ) {
-      granted = true;
-    }
-    // The user didn't grant permission, so return false.
-    return granted;
+export async function verifyPermission(fileHandle, readWrite) {
+  const options = {};
+  if (readWrite) {
+    options.mode = "readwrite";
   }
+  let granted = false;
+  // Check if permission was already granted. If so, return true.
+  if ((await fileHandle.queryPermission(options)) === "granted") {
+    granted = true;
+  }
+  // Request permission. If the user grants permission, return true.
+  if (
+    !granted && (await fileHandle.requestPermission(options)) === "granted"
+  ) {
+    granted = true;
+  }
+  // The user didn't grant permission, so return false.
+  return granted;
 }
