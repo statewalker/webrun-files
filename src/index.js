bb2f536e792c2cbcfcd15a1022f77dd8891218b3
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2023-09-16 07:36:43 +0200

    fix files imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export { default as FilesApi } from "./FilesApi.js";
 export { default as MemFilesApi } from "./MemFilesApi.js";
 export { default as NodeFilesApi } from "./NodeFilesApi.js";
-export { default as BrowserFilesApi } from "./BrowserFilesApi-x.js";
+export { default as BrowserFilesApi } from "./BrowserFilesApi.js";
 export { default as normalizePath } from "./normalizePath.js";
 export { default as addFilesApiLogger } from "./addFilesApiLogger.js";
 
